aff35d5983510045af5cbd1e49165d83cbabdfa1
Gerson Cardoso Filho <gersoncfilho@mac.com>
2018-01-03 05:55:47 -0200

    Criacao adapter

--- app/src/main/java/br/com/grupoccaa/projetolivroprofessor/adapters/EstanteAdapter.java
+++ app/src/main/java/br/com/grupoccaa/projetolivroprofessor/adapters/EstanteAdapter.java
@@ -3,10 +3,12 @@ package br.com.grupoccaa.projetolivroprofessor.adapters;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import br.com.grupoccaa.projetolivroprofessor.models.Publicacao;
 
@@ -28,16 +30,27 @@ public class EstanteAdapter extends RecyclerView.Adapter<EstanteAdapter.ViewHold
 
         public ViewHolder(View itemView) {
             super(itemView);
-            this.imagePublicacao = (ImageView) itemView.findViewById(R.id.);
-            this.textViewTituloPublicacao = textViewTituloPublicacao;
-            this.cardView = cardView;
+
         }
     }
 
+    public EstanteAdapter(ArrayList<Publicacao> datasetPublicacoes) {
+        this.datasetPublicacoes = datasetPublicacoes;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ImageView
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
 
 
-
-
-
-
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
